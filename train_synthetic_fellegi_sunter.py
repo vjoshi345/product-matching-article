9550a6f2e3183c721158419ce12a2334791af277
Varun Joshi <vjoshi345@gmail.com>
2024-04-20 18:23:17 -0400

    fs model code

---
+++ train_synthetic_fellegi_sunter.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import splink.duckdb.comparison_library as cl
+import splink.duckdb.comparison_template_library as ctl
+from splink.datasets import splink_datasets, splink_dataset_labels
+from splink.duckdb.linker import DuckDBLinker
+from splink.duckdb.blocking_rule_library import block_on
+
+file_dir = 'C:\WalmartLabs\EntityResolutionInSearch'
+link_file_path = 'C:\WalmartLabs\EntityResolutionInSearch\synthetic_dataset\product_link_data_A_B_cleaned.txt'
+file_path_a = 'C:\WalmartLabs\EntityResolutionInSearch\synthetic_dataset\product_data_A_cleaned.txt'
+file_path_b = 'C:\WalmartLabs\EntityResolutionInSearch\synthetic_dataset\product_data_B_cleaned.txt'
+labels_file_path = 'C:\WalmartLabs\EntityResolutionInSearch\synthetic_dataset\product_link_data_labels_full.csv'
+
+df_a = pd.read_csv(file_path_a, sep='|')
+print(df_a.shape)
+print(df_a.head())
+
+df_b = pd.read_csv(file_path_b, sep='|')
+print(df_b.shape)
+print(df_b.head())
+
+df_labels = pd.read_csv(labels_file_path)
+print(df_labels.shape)
+print(df_labels.dtypes)
+
+settings = {
+    "link_type": "link_only",
+    "comparisons": [
+        ctl.name_comparison("Title"),
+        ctl.name_comparison("Product_Type"),
+        ctl.name_comparison("Brand")
+    ],
+}
+print(settings['comparisons'][3].human_readable_description)
+linker = DuckDBLinker([df_a, df_b], settings, input_table_aliases=["df_left", "df_right"])
+linker.completeness_chart(cols=["Title", "Product_Type", "Brand", "Price"])
+
+deterministic_rules = [
+    "levenshtein(l.Title, r.Title) <= 1",
+]
+
+linker.estimate_probability_two_random_records_match(deterministic_rules, recall=0.7)
+
+linker.estimate_u_using_random_sampling(max_pairs=1e6, seed=1)
+
+session_title = linker.estimate_parameters_using_expectation_maximisation(block_on("Title"))
+session_brand = linker.estimate_parameters_using_expectation_maximisation(block_on("Brand"))
+
+# Use the FS model to predict links
+results = linker.predict(threshold_match_probability=0.90)
+out_df = results.as_pandas_dataframe()
+out_df.to_csv(file_dir+'\\results\output_fs_th_090.txt', sep='|', index=False)
+
+# Evaluation
+labels_table = linker.register_labels_table(df_labels)
+roc_table = linker.truth_space_table_from_labels_table(labels_table)
+roc_table_df = roc_table.as_pandas_dataframe()
+print(roc_table_df.shape)
+roc_table_df.to_csv(file_dir+'\\results\\roc_table_w_price.csv', index=False)
